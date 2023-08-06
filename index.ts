925843340d4ffcdf6549eac421a9114b92d4c70a
vindard <1457480-vindard@users.noreply.replit.com>
2023-08-06 17:46:58 +0000

    test: implement FizzBuzz case

--- index.ts
+++ index.ts
@@ -9,7 +9,8 @@
 const number = Number(process.argv[2])
 const output = console.log
 for (let i = 1; i <= number; i++) {
-  if (i === 3) output("Fizz")
-  else if (i == 5) output("Buzz")
+  if (i % 3 == 0 && i % 5 == 0) output("FizzBuzz")
+  else if (i % 3 === 0) output("Fizz")
+  else if (i % 5 == 0) output("Buzz")
   else output(i)
 }
